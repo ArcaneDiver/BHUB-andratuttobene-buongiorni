d89e1da8afc9b5ee23db4527bcc81121fe6b6551
ArcaneDiver <michele.dellamea@bearzi.org>
2020-04-04 22:32:41 +0200

    changes

---
+++ buongiorni.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Andràtuttobene
+Plugin URI: https://github.com/ArcaneDiver/BHUB-andratuttobene-buongiorni
+Description: Integrate Buongiorno post-type
+Version: 1.0
+Author: Michele Della Mea
+Author URI: https://github.com/ArcaneDiver
+*/
